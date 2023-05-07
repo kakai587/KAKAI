973e19c0a287a55bae06936ceb00d52c8b8618bd
kakai <julykirkxy@gmail.com>
2023-05-07 20:10:11 +0300

    make some changes

--- src/components/message.js
+++ src/components/message.js
@@ -4,7 +4,7 @@ import Message from './Components/Message';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!Created my 1<sup>st</sup> react app</h1>
+      <h1>Hello World! Created my 1<sup>st</sup> react app</h1>
       <Message/>
     </div>
   );
